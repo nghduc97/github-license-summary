cdb8f2d826e824cabbeb8e6fd3094e9a682e4892
Phạm Nguyễn Ngọc Biên <pham.bien10@gmail.com>
2017-10-26 16:36:20 +0700

    Add loader

--- src/main.js
+++ src/main.js
@@ -16,19 +16,51 @@ const getSummary = async (url) => {
   return getAncestor(iconElement, 2)
 }
 
+const createLoaderElement = () => {
+  const loaderElement = document.createElement('div')
+  loaderElement.className = 'Box desktop-license-summary-loader'
+
+  const loaderImgElement = document.createElement('img')
+  loaderImgElement.className = 'octospinner'
+  loaderImgElement.src = 'https://assets-cdn.github.com/images/spinners/octocat-spinner-128.gif'
+  loaderImgElement.width = 64
+  loaderImgElement.height = 64
+  loaderElement.appendChild(loaderImgElement)
+
+  return loaderElement
+}
+
 const loadDesktopView = async () => {
   // Find license summary element from the law icon
   const iconElement = document.getElementsByClassName('octicon-law').item(0)
   if (iconElement) {
     try {
-      const licenseElement = getAncestor(iconElement, 1)
-      const url = 'https://github.com' + licenseElement.getAttribute('href')
-      const summaryElement = await getSummary(url)
-      summaryElement.classList.add('desktop-license-summary')
-
-      // insert element
+      // Get file-navigation element and its ancestor
       const fileNavElement = document.getElementsByClassName('file-navigation').item(0)
-      getAncestor(fileNavElement, 1).insertBefore(summaryElement, fileNavElement)
+      const ancestor = getAncestor(fileNavElement, 1)
+      const licenseElement = getAncestor(iconElement, 1)
+
+      // Check if license information is available
+      if (!licenseElement || !licenseElement.getAttribute('href')) {
+        return
+      }
+
+      // Get license information url
+      const url = 'https://github.com' + licenseElement.getAttribute('href')
+
+      // Create summary element
+      const parentSummaryElement = document.createElement('div')
+      parentSummaryElement.className = 'desktop-license-summary'
+      ancestor.insertBefore(parentSummaryElement, fileNavElement)
+
+      // Create loader element
+      const loaderElement = createLoaderElement()
+      parentSummaryElement.appendChild(loaderElement)
+
+      // Get and insert license summary element
+      const summaryElement = await getSummary(url)
+      loaderElement.style.display = 'none'
+      parentSummaryElement.appendChild(summaryElement)
     } catch (err) {
       console.error(err)
     }
